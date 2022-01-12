93b31214d220d248ade8542b9be1d4ff14df172d
liptonzuma <solomonboateng231@gmail.com>
2022-01-12 12:09:05 +0000

    setup

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const api = require('./src/routes/api');
 
 const port = process.env.PORT || 8000;
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Welcome</h1>')
+})
 
 app.listen(port,()=>{
     console.log(`Running on port ${port}`);
