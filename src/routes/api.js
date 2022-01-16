5794c3443ac61aaeca9f8a4228d092616754fa87
liptonzuma <solomonboateng231@gmail.com>
2022-01-16 12:36:31 +0000

    mongodb

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../schemas/user');
+const users = require('../schemas/user');
 
 router.get('/',(req,res)=>{
     res.send('Welcome');
 })
 
 router.post('/',(req,res)=>{
-    user.create(req.body)
+    users.create(req.body)
     .then(data=>res.send(data))
     .catch(err=>res.send(err.message));
 })
  
 router.put('/:id',(req,res)=>{
     const {id} = req.params;
-    user.findByIdAndUpdate(id,req.body)
+    users.findByIdAndUpdate(id,req.body)
     .then(()=> {
-        user.findById(id)
+        users.findById(id)
         .then(data=>res.send(data));
     });
 })
